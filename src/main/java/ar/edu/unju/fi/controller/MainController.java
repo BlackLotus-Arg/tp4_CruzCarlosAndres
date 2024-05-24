e26f5e5bf7be838a25e0bc6c65eafaff93ef6125
Andres Cruz <andres_013c@yahoo.com.ar>
2024-05-24 00:35:59 -0300

    Se crea el main controller con la ruta / y se enlaza al index.html

---
+++ src/main/java/ar/edu/unju/fi/controller/MainController.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+	@RequestMapping(path = "/", method = RequestMethod.GET)
+	public String getIndex() {
+		return "index";
+	}
+}
