d4edf481ff02d4de206185733647a79310157172
Andres Cruz <andres_013c@yahoo.com.ar>
2024-05-24 00:16:52 -0300

    Se agregan los atributos a los modelos, junto a constructures, getters y setters

--- src/main/java/ar/edu/unju/fi/model/Carrera.java
+++ src/main/java/ar/edu/unju/fi/model/Carrera.java
@@ -1,5 +1,44 @@
 package ar.edu.unju.fi.model;
 
 public class Carrera {
-
+	private Integer codigo;
+	private String nombre;
+	private Integer duracion;
+	private String estado;
+	public Carrera(Integer codigo, String nombre, Integer duracion, String estado) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.duracion = duracion;
+		this.estado = estado;
+	} 
+	public Carrera() {
+		
+	}
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Integer getDuracion() {
+		return duracion;
+	}
+	public void setDuracion(Integer duracion) {
+		this.duracion = duracion;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
 }
